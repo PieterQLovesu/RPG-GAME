47167c39297e29121e8df30bee8bad6801ccb1f4
PieterQLovesu <pieter444@wp.pl>
2017-02-07 15:19:03 +0100

    Add files via upload

---
+++ gra rpg/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picturePotionEQ = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ButtonDown = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ButtonPick = new System.Windows.Forms.Button();
+            this.buttonAttack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBarHp = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePotionEQ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picturePotionEQ
+            // 
+            this.picturePotionEQ.BackColor = System.Drawing.Color.Transparent;
+            this.picturePotionEQ.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picturePotionEQ.BackgroundImage")));
+            this.picturePotionEQ.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picturePotionEQ.Location = new System.Drawing.Point(79, 319);
+            this.picturePotionEQ.Name = "picturePotionEQ";
+            this.picturePotionEQ.Size = new System.Drawing.Size(50, 50);
+            this.picturePotionEQ.TabIndex = 12;
+            this.picturePotionEQ.TabStop = false;
+            this.picturePotionEQ.Visible = false;
+            this.picturePotionEQ.Click += new System.EventHandler(this.picturePotionEQ_Click);
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox14.Image = global::WindowsFormsApplication1.Properties.Resources.potion_blue;
+            this.pictureBox14.Location = new System.Drawing.Point(135, 319);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox14.TabIndex = 13;
+            this.pictureBox14.TabStop = false;
+            this.pictureBox14.Visible = false;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(399, 319);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(30, 28);
+            this.buttonUp.TabIndex = 14;
+            this.buttonUp.Text = "▲";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 28);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "◄";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ButtonDown
+            // 
+            this.ButtonDown.Location = new System.Drawing.Point(399, 353);
+            this.ButtonDown.Name = "ButtonDown";
+            this.ButtonDown.Size = new System.Drawing.Size(30, 28);
+            this.ButtonDown.TabIndex = 16;
+            this.ButtonDown.Text = "▼";
+            this.ButtonDown.UseVisualStyleBackColor = true;
+            this.ButtonDown.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(432, 353);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 28);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "►";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ButtonPick
+            // 
+            this.ButtonPick.Location = new System.Drawing.Point(468, 353);
+            this.ButtonPick.Name = "ButtonPick";
+            this.ButtonPick.Size = new System.Drawing.Size(75, 23);
+            this.ButtonPick.TabIndex = 20;
+            this.ButtonPick.Text = "Pick";
+            this.ButtonPick.UseVisualStyleBackColor = true;
+            this.ButtonPick.Visible = false;
+            this.ButtonPick.Click += new System.EventHandler(this.ButtonPick_Click);
+            // 
+            // buttonAttack
+            // 
+            this.buttonAttack.Location = new System.Drawing.Point(468, 324);
+            this.buttonAttack.Name = "buttonAttack";
+            this.buttonAttack.Size = new System.Drawing.Size(75, 23);
+            this.buttonAttack.TabIndex = 21;
+            this.buttonAttack.Text = "Attack";
+            this.buttonAttack.UseVisualStyleBackColor = true;
+            this.buttonAttack.Click += new System.EventHandler(this.buttonAttack_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(631, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "HPBat";
+            // 
+            // progressBarHp
+            // 
+            this.progressBarHp.Location = new System.Drawing.Point(634, 285);
+            this.progressBarHp.Maximum = 10;
+            this.progressBarHp.Name = "progressBarHp";
+            this.progressBarHp.Size = new System.Drawing.Size(88, 13);
+            this.progressBarHp.TabIndex = 23;
+            this.progressBarHp.Value = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(380, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "HpGracz";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(435, 269);
+            this.progressBar1.Maximum = 30;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(88, 16);
+            this.progressBar1.TabIndex = 25;
+            this.progressBar1.Value = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.nowamapa;
+            this.ClientSize = new System.Drawing.Size(747, 399);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBarHp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAttack);
+            this.Controls.Add(this.ButtonPick);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.ButtonDown);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.picturePotionEQ);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picturePotionEQ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picturePotionEQ;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ButtonDown;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button ButtonPick;
+        private System.Windows.Forms.Button buttonAttack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBarHp;
+        private System.Windows.Forms.Label label2;
+        // private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
